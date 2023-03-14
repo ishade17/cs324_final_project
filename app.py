947a7da5a41725d5ae38085fb520d543d4149183
selenasun1618 <selenas@stanford.edu>
2023-03-14 05:39:44 -0700

    added natural language, rendering opt, minor fixes

--- app.py
+++ app.py
@@ -1,45 +1,67 @@
 import streamlit as st
 import build_graph
 import streamlit.components.v1 as components
+import openai
+
+openai.api_key = "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"
 
 def load_UI():
     st.title('Graph Visualization of Career Paths')
 
-@st.cache_data
-def construct_graph(query):
-    net = build_graph.pyvis_graph(query)
-    return net
+@st.cache_resource
+def construct_graph():
+    net, node_counter, connection_counter = build_graph.pyvis_graph()
+    return net, node_counter, connection_counter
 
 def query_preprocessing(query):
     query = query.lower()
     query.replace("  ", " ")
     return query
 
-def load_search(query, net):
+def call_chatgpt(prompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", 
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return completion.choices[0].message.content
+
+def load_search(query, net, node_counter, connection_counter):
     _, relevant_people, highlighted_in_nodes, higlighted_out_nodes = build_graph.find_relevant_nodes(query)
     all_nodes = set(net.get_nodes())
     if query != "":
-        if query in all_nodes:
+        if query in " ".join(all_nodes):
             st.subheader(f"Career paths containing {query}: {len(relevant_people)}")
             st.write("In-paths in red. Out-paths in blue.")
-            net = build_graph.color_nodes(net,  query, highlighted_in_nodes, higlighted_out_nodes, in_network=True)
+            net = build_graph.color_nodes_edges(net,  query, highlighted_in_nodes, higlighted_out_nodes, in_network=True)
             html = net.save_graph("career_graph.html")
         else:
             st.write(f"No results found. Try a different search.")
-            net = build_graph.color_nodes(net,  query, highlighted_in_nodes, higlighted_out_nodes)
+            net = build_graph.color_nodes_edges(net,  query, highlighted_in_nodes, higlighted_out_nodes)
             html = net.save_graph("career_graph.html")
 
         with open('career_graph.html', 'r') as HtmlFile:
-            components.html(HtmlFile.read(), height=435)
+            components.html(HtmlFile.read(), height=500)
+
+        # find most common before & after nodes
+        most_common_pre_node, most_common_post_node, _, _, percent_pre, percent_post = build_graph.most_common_path(net, query, node_counter, connection_counter)
+
+        query = query.capitalize()
+        prompt = f"Give 3 bullet points of advice on how to get to {query}, given that the most common working experience before this is {most_common_pre_node}."
+        generation = call_chatgpt(prompt)
+        st.subheader(f"Advice on getting to {query}:")
+        st.write(f"The most common experience before {query} is {most_common_pre_node.capitalize()}, with {round(percent_pre, 3)*100}% of {query} coming from {most_common_pre_node.capitalize()} directly. After {query}, {round(percent_post, 3)*100}% go to {most_common_post_node.capitalize()}.")
+        st.write(f"\nHere's some advice for getting to {query}:\n")
+        st.write(generation)
 
         if len(relevant_people) != 0:
-            st.subheader(f"Most notable people at {query}:")
+            st.subheader(f"Notable people at {query}:")
             for p in relevant_people:
                 st.markdown("- " + p)
+        
 
 if __name__ == '__main__':
     load_UI()
     query = st.text_input('Search by univerity or company name')
     query = query_preprocessing(query)
-    net = construct_graph(query)
-    load_search(query,net)
+    net, node_counter, connection_counter = construct_graph()
+    load_search(query,net, node_counter, connection_counter)
--- build_graph.py
+++ build_graph.py
@@ -39,7 +39,7 @@ def networkx_graph():
             connection = (place_nodes[i], place_nodes[i + 1])
             connection_counter[connection] += 1
             node_counter[place_nodes[i]] += 1
-            G.add_edge(place_nodes[i], place_nodes[i + 1], color=('red', 'blue'))
+            G.add_edge(place_nodes[i], place_nodes[i + 1])
         node_counter[place_nodes[-1]] += 1
 
     all_nodes = set(all_nodes)
@@ -54,43 +54,95 @@ def find_relevant_nodes(query):
     highlighted_in_nodes = []
     for person, info in data.items():
         place_nodes = [e[0].lower() for e in info]
-        if query != "" and query in place_nodes:
-            split_index = place_nodes.index(query)
-            relevant_people.append(person)
-            highlighted_in_nodes.extend(place_nodes[:split_index])
-            highlighted_out_nodes.extend(place_nodes[split_index+1:])
-    
+        concat_places = " ".join(place_nodes)
+        if query != "" and query in concat_places:
+
+            # search for index of query in place_nodes
+            for count, node in enumerate(place_nodes):
+                if query in node:
+                    split_index = count
+                    relevant_people.append(person)
+                    highlighted_in_nodes.extend(place_nodes[:split_index])
+                    highlighted_out_nodes.extend(place_nodes[split_index+1:])
+                    break
+            
     highlighted_in_nodes = set(highlighted_in_nodes)
     highlighted_out_nodes = set(highlighted_out_nodes)
 
     return query, relevant_people, highlighted_in_nodes, highlighted_out_nodes
 
 
-def color_nodes(net, query, highlighted_in_nodes, highlighted_out_nodes, in_network=False):
+def color_nodes_edges(net, query, highlighted_in_nodes, highlighted_out_nodes, in_network=False):
     print("coloring nodes....")
     print("query: ", query)
     if in_network:
         # highlighting relevant nodes
         for node in net.nodes:
-            if node['id'] == query:
-                print("HERE: ", node['id'])
-                node['shape'] = 'star'
+            if query in node['id']:
                 node['color'] = 'yellow'
+                node['shape'] = 'star'
+                node['size'] = 50
 
             elif node['id'] in highlighted_in_nodes:
                 node['color'] = 'red'
+                node['shape'] = 'dot'
+                node['size'] = 1 # HANNA ATTN
+
             elif node['id'] in highlighted_out_nodes:
                 node['color'] = 'blue'
+                node['shape'] = 'dot'
+                node['size'] = 1 # HANNA ATTN
+
             else:
                 node['color'] = "#ddddff"
+                node['shape'] = 'dot'
+                node['size'] = 1 # HANNA ATTN
+        
+        # highlighting relevant edges
+        for edge in net.edges:
+            source_id = edge['from']
+            if source_id in highlighted_in_nodes:
+                edge['color'] = 'red'
+            elif source_id in highlighted_out_nodes:
+                edge['color'] = 'blue'
+            else:
+                edge['color'] = "#ddddff"
+
     else:
         for node in net.nodes:
             node['color'] = "#ddddff"
+            node['shape'] = 'dot'
+            node['size'] = 1
 
     return net
 
+def most_common_path(net, query, node_counter, connection_counter):
+    adj_list = net.get_adj_list()
+    most_common_post = 0
+    most_common_pre = 0
+    most_common_pre_node = ""
+    most_common_post_node = ""
+    queried_node = ""
+    for node in net.nodes:
+        if query in node['id']:
+            queried_node = node['id']
+            break
+    for adj_node in adj_list[queried_node]:
+        edge_val = connection_counter[(queried_node, adj_node)]
+        for edge in net.edges:
+            if edge['from'] == queried_node and edge['to'] == adj_node:
+                if edge_val > most_common_post:
+                    most_common_post = edge_val
+                    most_common_post_node = adj_node
+            elif edge['to'] == queried_node and edge['from'] == adj_node:
+                if edge_val > most_common_pre:
+                    most_common_pre = edge_val
+                    most_common_pre_node = adj_node
+    percent_post = most_common_post / node_counter[queried_node]
+    percent_pre = most_common_pre / node_counter[queried_node]
+    return most_common_pre_node, most_common_post_node, most_common_pre, most_common_post, percent_pre, percent_post
 
-def pyvis_graph(query):
+def pyvis_graph():
     # Take Networkx graph and translate it to a PyVis graph format
 
     G, _, connection_counter, node_counter = networkx_graph()
@@ -101,18 +153,19 @@ def pyvis_graph(query):
                   spring_length=110, spring_strength=0.10,
                   damping=0.95)
 
+    # set smooth edges to avoid re-rendering
+    net.set_edge_smooth("Dynamic")
+
     # adding node size and labels based on number of people with connection
     for edge in net.edges:
         source_id = edge['from']
         target_id = edge['to']
         weight = connection_counter[(source_id, target_id)]
         edge['title'] = f"{source_id} to {target_id}: {weight} people"
-        if source_id == query:
-            edge['color'] = 'blue'
-            
+
     for node in net.nodes:
         num_people = node_counter[node['id']]
-        node['size'] = num_people
+        # node['size'] = num_people * 0.5 + 1 # set node size, HANNA ATTN
         node['title'] = f"People at {node['id']}: {num_people}"
         
-    return net
+    return net, node_counter, connection_counter
